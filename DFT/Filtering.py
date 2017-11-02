7c70c20b2741f5a61d9af0a7100a55ee220cb2cc
tbnguyen <buuty08@gmail.com>
2017-11-02 15:38:17 -0500

    Finished part 2

--- DFT/Filtering.py
+++ DFT/Filtering.py
@@ -1,7 +1,7 @@
 # For this part of the assignment, You can use inbuilt functions to compute the fourier transform
 # You are welcome to use fft that are available in numpy and opencv
 import numpy as np
-from cmath import exp
+import cv2
 
 class Filtering:
     image = None
@@ -34,7 +34,6 @@ class Filtering:
         self.cutoff = cutoff
         self.order = order
 
-
     def get_ideal_low_pass_filter(self, shape, cutoff):
         """Computes a Ideal low pass mask
         takes as input:
@@ -46,20 +45,19 @@ class Filtering:
         N = shape.shape[0]
 
         # Create empty mask array
-        mask = np.zeros(shape=(N, N), dtype=float)
+        mask = np.array(shape, dtype=float)
 
-        for row in range(N):
-            for col in range(N):
-                distance = np.sqrt((np.power((row - N / 2), 2)) + (np.power((col - N / 2), 2)))
+        for u in range(N):
+            for v in range(N):
+                distance = np.sqrt((np.power((u - (N / 2)), 2)) + (np.power((v - (N / 2)), 2)))
 
                 # Check distance with cutoff
                 if (distance <= cutoff):
-                    mask[row, col] = 1
+                    mask[u, v] = 1
                 elif (distance > cutoff):
-                    mask[row, col] = 0
+                    mask[u, v] = 0
 
         return mask
-
 
     def get_ideal_high_pass_filter(self, shape, cutoff):
         """Computes a Ideal high pass mask
@@ -74,15 +72,15 @@ class Filtering:
         # Create empty mask array
         mask = np.zeros(shape=(N, N), dtype=float)
 
-        for row in range(N):
-            for col in range(N):
-                distance = np.sqrt((np.power((row - N / 2), 2)) + (np.power((col - N / 2), 2)))
+        for u in range(N):
+            for v in range(N):
+                distance = np.sqrt((np.power((u - (N / 2)), 2)) + (np.power((v - (N / 2)), 2)))
 
                 # Check distance with cutoff
                 if (distance <= cutoff):
-                    mask[row, col] = 0
+                    mask[u, v] = 0
                 elif (distance > cutoff):
-                    mask[row, col] = 1
+                    mask[u, v] = 1
 
         return mask
 
@@ -100,16 +98,15 @@ class Filtering:
         # Create empty mask array
         mask = np.zeros(shape=(N, N), dtype=float)
 
-        for row in range(N):
-            for col in range(N):
-                distance = np.sqrt((np.power((row - N / 2), 2)) + (np.power((col - N / 2), 2)))
+        for u in range(N):
+            for v in range(N):
+                distance = np.sqrt((np.power((u - (N / 2)), 2)) + (np.power((v - (N / 2)), 2)))
                 sum = 1 + np.power((distance / cutoff), (2 * order))
                 value = 1 / sum
 
-                mask[row, col] = value
+                mask[u, v] = value
 
         return mask
-
 
     def get_butterworth_high_pass_filter(self, shape, cutoff, order):
         """Computes a butterworth high pass mask
@@ -125,13 +122,18 @@ class Filtering:
         # Create empty mask array
         mask = np.zeros(shape=(N, N), dtype=float)
 
-        for row in range(N):
-            for col in range(N):
-                distance = np.sqrt((np.power((row - N / 2), 2)) + (np.power((col - N / 2), 2)))
+        for u in range(N):
+            for v in range(N):
+                distance = np.sqrt((np.power((u - (N / 2)), 2)) + (np.power((v - (N / 2)), 2)))
+
+                # Check if distance is less than 0
+                if (distance <= 0):
+                    distance = 1
+
                 sum = 1 + np.power((cutoff / distance), (2 * order))
                 value = 1 / sum
 
-                mask[row, col] = value
+                mask[u, v] = value
 
         return mask
 
@@ -148,12 +150,12 @@ class Filtering:
         # Create empty mask array
         mask = np.zeros(shape=(N, N), dtype=float)
 
-        for row in range(N):
-            for col in range(N):
-                distance = np.sqrt((np.power((row - N / 2), 2)) + (np.power((col - N / 2), 2)))
-                value = exp(-1 * np.power(distance, 2) / (2 * np.power(cutoff)))
+        for u in range(N):
+            for v in range(N):
+                distance = np.sqrt((np.power((u - (N / 2)), 2)) + (np.power((v - (N / 2)), 2)))
+                value = np.exp((-1 * np.power(distance, 2)) / (2 * np.power(cutoff, 2)))
 
-                mask[row, col] = value
+                mask[u, v] = value
 
         return mask
 
@@ -170,12 +172,12 @@ class Filtering:
         # Create empty mask array
         mask = np.zeros(shape=(N, N), dtype=float)
 
-        for row in range(N):
-            for col in range(N):
-                distance = np.sqrt((np.power((row - N / 2), 2)) + (np.power((col - N / 2), 2)))
-                value = 1 - (exp(-1 * np.power(distance, 2) / (2 * np.power(cutoff))))
+        for u in range(N):
+            for v in range(N):
+                distance = np.sqrt((np.power((u - (N / 2)), 2)) + (np.power((v - (N / 2)), 2)))
+                value = 1 - (np.exp(-1 * np.power(distance, 2) / (2 * np.power(cutoff, 2))))
 
-                mask[row, col] = value
+                mask[u, v] = value
 
         return mask
 
@@ -191,60 +193,71 @@ class Filtering:
         N = image.shape[0]
 
         # 1. Full contrast stretch (fsimage)
-        processed = np.copy(image)
+        processed = np.zeros(shape=(N, N), dtype=float)
+
+        min = image.min()
+        max = image.max()
 
         for row in range(N):
             for col in range(N):
-                value = ((image[row, col] - image.min()) / (image.max() - image.min())) * 255
+                pixel = image[row, col]
+                value = ((pixel - min) / (max - min)) * 255
+
                 processed[row, col] = value
-
-        # 2. take negative (255 - fsimage)
-        for row in range(N):
-            for col in range(N):
-                processed[row, col] = (255 - processed[row, col])
-
-        # Convert to uint8 type
-        processed = processed.astype(np.uint8)
 
         return processed
 
-
     def filtering(self):
         """Performs frequency filtering on an input image
-        returns a filtered image, magnitude of DFT, magnitude of filtered DFT        
+        returns a filtered image, magnitude of DFT, magnitude of filtered DFT
         ----------------------------------------------------------
         You are allowed to used inbuilt functions to compute fft
         There are packages available in numpy as well as in opencv
         Steps:
         """
-        #Steps:
-        #1. Compute the fft of the image
+        # Steps:
+
+        # 1. Compute the fft of the image
         fftImage = np.fft.fft2(self.image)
 
-        #2. shift the fft to center the low frequencies
-        shiftedImage = np.fft.fftshift(fftImage)
+        # 2. shift the fft to center the low frequencies
+        shifted_fft = np.fft.fftshift(fftImage)
 
-        #3. get the mask (write your code in functions provided above) the functions can be called by self.filter(shape, cutoff, order)
-        mask = self.get_ideal_low_pass_filter(self.image, self.cutoff)
+        # Save magnitude of DFT
+        dft_magnitude = 10 * np.log(np.abs(shifted_fft))
+        dft_image = dft_magnitude.astype('uint8')
+        cv2.imwrite('output\Magnitude_of_DFT.jpg', dft_image)
 
-        #4. filter the image frequency based on the mask (Convolution theorem)
-        filtered = np.dot(mask, shiftedImage)
+        # 3. get the mask (write your code in functions provided above) the functions can be called by self.filter(shape, cutoff, order)
+        mask = self.filter(self.image)
 
-        #5. compute the inverse shift
+        # 4. filter the image frequency based on the mask (Convolution theorem)
+        filtered = mask * shifted_fft
+
+        # 5. compute the inverse shift
         inverse_shift = np.fft.ifftshift(filtered)
 
-        #6. compute the inverse fourier transform
+        # 6. compute the inverse fourier transform
         inverse_fft = np.fft.ifft2(inverse_shift)
 
-        #7. compute the magnitude
+        # 7. compute the magnitude
         computed_magnitude = np.abs(inverse_fft)
 
-        #8. You will need to do a full contrast stretch on the magnitude and depending on the algorithm you may also need to
+        # Save magnitude of filtered dft
+        filtered_magnitude = computed_magnitude.astype('uint8')
+        cv2.imwrite('output\Magnitude_of_Filtered_DFT.jpg', filtered_magnitude)
+
+        # 8. You will need to do a full contrast stretch on the magnitude and depending on the algorithm you may also need to
+        # take negative of the image to be able to view it (use post_process_image to write this code)
+        # Note: You do not have to do zero padding as discussed in class, the inbuilt functions takes care of that
+        # filtered image, magnitude of DFT, magnitude of filtered DFT: Make sure all images being returned have grey scale full contrast stretch and dtype=uint8
         processed = self.post_process_image(computed_magnitude)
 
-        #take negative of the image to be able to view it (use post_process_image to write this code)
-        #Note: You do not have to do zero padding as discussed in class, the inbuilt functions takes care of that
-        #filtered image, magnitude of DFT, magnitude of filtered DFT: Make sure all images being returned have grey scale full contrast stretch and dtype=uint8
+        # Save filtered image
+        processed = processed.astype('uint8')
+        cv2.imwrite('output\Filtered_Image.jpg', processed)
 
 
-        return processed
+
+        # filtered image, magnitude of the DFT and magnitude of filtered dft
+        return [processed, dft_image, filtered_magnitude]
